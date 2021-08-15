58edbcd224ffa8d6a3e2db3b66c5ec299c14dfd5
ruase <69146227+ruase@users.noreply.github.com>
2021-08-15 22:56:14 +0900

    in-out log

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,28 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+    
+@bot.event
+async def on_vc_start(member, channel):
+    print(f"{member.name}が{channel.name}でボイスチャットを開始しました。")
 
+
+@bot.event
+async def on_vc_end(member, channel):
+    print(f"{member.name}が{channel.name}のボイスチャットを終了しました。")
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel != after.channel:
+        if after.channel and len(after.channel.members) == 1:
+            # もし、ボイスチャットが開始されたら
+            bot.dispatch("vc_start", member, after.channel)  # 発火！
+
+        if before.channel and len(before.channel.members) == 0:
+            # もし、ボイスチャットが終了したら
+            bot.dispatch("vc_end", member, before.channel)  # 発火！
+
+            
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
