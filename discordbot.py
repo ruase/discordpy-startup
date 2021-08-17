0a21c613d50228c0c1ba61bb5c01656a28cf769e
るあせっ <69146227+ruase@users.noreply.github.com>
2021-08-18 02:25:45 +0900

    update delete-log to admins channel

--- discordbot.py
+++ discordbot.py
@@ -99,6 +99,15 @@ async def on_message_delete(message):
         embed.set_footer(icon_url=message.author.avatar_url, text=message.author.display_name)
         channel = message.guild.get_channel(876859685146345504)
         await channel.send(embed=embed)
+    else:
+        now = jst()
+        embed = discord.Embed(title="メッセージ削除", color=discord.Color.red())
+        embed.add_field(name="メッセージ", value=message.content, inline=False)
+        embed.add_field(name="時刻", value=now.strftime('%Y /%m / %d　 %H : %M : %S'), inline=False)
+        embed.add_field(name="チャンネル", value=message.channel.mention, inline=False)
+        embed.set_footer(icon_url=message.author.avatar_url, text=message.author.display_name)
+        channel = message.guild.get_channel(876536099172925461)
+        await channel.send(embed=embed)
 
     
     
