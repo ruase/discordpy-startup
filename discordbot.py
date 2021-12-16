595f4e3562dfbe0fd47a080b3693aeec69321c27
るあせっ <69146227+ruase@users.noreply.github.com>
2021-12-16 21:33:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,14 @@ from discord.channel import VoiceChannel
 from discord.player import FFmpegPCMAudio
 from google.cloud import texttospeech
 from collections import deque
+from os import getenv
+import traceback
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 bot = commands.Bot(command_prefix='&')
 bot.remove_command("help")
