7960117a33282180140168c2fdb75a41d33f486d
るあせっ <69146227+ruase@users.noreply.github.com>
2021-12-16 21:32:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,12 +17,6 @@ bot = commands.Bot(command_prefix='&')
 bot.remove_command("help")
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
 
 @bot.command(name="ping")
 async def ping(ctx):
