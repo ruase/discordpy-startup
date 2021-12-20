9bee90e4546d77a37b992e63390ec390a1af18bb
るあせっ <69146227+ruase@users.noreply.github.com>
2021-12-21 08:47:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -125,8 +125,6 @@ async def reminder():
     now_jst = jst()
     now_weekday = now_jst.weekday()
     now = now_jst.strftime("%H:%M")
-    await channel.send(now)
-    await channel.send(now_weekday)
     if now == "21:00":
         await channel.send("<@&921002690987823114>チャレライやった？")
         if now_weekday == 2 or now_weekday == 6:
